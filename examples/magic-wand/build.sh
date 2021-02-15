1b95d9513f18fefe07a25ec4ebd776253fccd7ca
Dawid Wojciechowski <dwojciechowski@antmicro.com>
2021-02-15 15:27:26 +0100

    Remove clone repo from building scripts

--- examples/magic-wand/build.sh
+++ examples/magic-wand/build.sh
@@ -1,6 +1,7 @@
+[ -z "$TENSORFLOW_PATH" ] && export TENSORFLOW_PATH=tensorflow-arduino-examples/tensorflow
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 bin/arduino-cli core install arduino:mbed
 bin/arduino-cli lib install Arduino_LSM9DS1@1.1.0
-git clone --quiet https://github.com/antmicro/tensorflow-arduino-examples.git && cp -r tensorflow-arduino-examples/tensorflow $HOME/Arduino/libraries
+mkdir -p $HOME/Arduino/libraries && cp -r $TENSORFLOW_PATH $HOME/Arduino/libraries
 sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/system_setup.cpp
 bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/magic_wand $HOME/Arduino/libraries/tensorflow/examples/magic_wand
