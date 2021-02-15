a2711dc9cd90e2894065b04bdcbfe333cdf574f8
Dawid Wojciechowski <dwojciechowski@antmicro.com>
2021-02-15 15:27:26 +0100

    Remove clone repo from building scripts

--- examples/hello-world/build.sh
+++ examples/hello-world/build.sh
@@ -1,5 +1,6 @@
+[ -z "$TENSORFLOW_PATH" ] && export TENSORFLOW_PATH=tensorflow-arduino-examples/tensorflow
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 bin/arduino-cli core install arduino:mbed
-git clone --quiet https://github.com/antmicro/tensorflow-arduino-examples.git && mkdir -p $HOME/Arduino/libraries && cp -r tensorflow-arduino-examples/tensorflow $HOME/Arduino/libraries
+mkdir -p $HOME/Arduino/libraries && cp -r $TENSORFLOW_PATH $HOME/Arduino/libraries
 sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/system_setup.cpp
 bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/hello_world $HOME/Arduino/libraries/tensorflow/examples/hello_world
--- examples/person-detection/build.sh
+++ examples/person-detection/build.sh
@@ -1,8 +1,9 @@
+[ -z "$TENSORFLOW_PATH" ] && export TENSORFLOW_PATH=tensorflow-arduino-examples/tensorflow
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 bin/arduino-cli core install arduino:mbed
 bin/arduino-cli lib install JPEGDecoder@1.8.0
 git clone --quiet https://github.com/ArduCAM/Arduino.git && cp -r Arduino/ArduCAM $HOME/Arduino/libraries
-git clone --quiet https://github.com/antmicro/tensorflow-arduino-examples.git && cp -r tensorflow-arduino-examples/tensorflow $HOME/Arduino/libraries
+mkdir -p $HOME/Arduino/libraries && cp -r $TENSORFLOW_PATH $HOME/Arduino/libraries
 sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/system_setup.cpp
  > $HOME/Arduino/libraries/JPEGDecoder/src/User_Config.h
 bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/person_detection $HOME/Arduino/libraries/tensorflow/examples/person_detection
