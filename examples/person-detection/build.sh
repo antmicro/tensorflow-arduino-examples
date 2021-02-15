e0effe7f7c4a3f106d2a3c3953160a678fd32be5
Dawid Wojciechowski <dwojciechowski@antmicro.com>
2021-02-15 14:06:33 +0100

    Change path for serial hack

--- examples/person-detection/build.sh
+++ examples/person-detection/build.sh
@@ -3,6 +3,6 @@ bin/arduino-cli core install arduino:mbed
 bin/arduino-cli lib install JPEGDecoder@1.8.0
 git clone --quiet https://github.com/ArduCAM/Arduino.git && cp -r Arduino/ArduCAM $HOME/Arduino/libraries
 git clone --quiet https://github.com/antmicro/tensorflow-arduino-examples.git && cp -r tensorflow-arduino-examples/tensorflow $HOME/Arduino/libraries
-sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/debug_log.cpp
+sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/system_setup.cpp
  > $HOME/Arduino/libraries/JPEGDecoder/src/User_Config.h
 bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/person_detection $HOME/Arduino/libraries/tensorflow/examples/person_detection
