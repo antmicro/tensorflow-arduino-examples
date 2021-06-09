1074eff21beea7f0db4cb570ae5370fd8ed7d210
Wojciech Meller <wmeller@antmicro.com>
2021-06-09 17:45:39 +0200

    Change tensorflow to tflite-micro

--- examples/magic-wand/build.sh
+++ examples/magic-wand/build.sh
@@ -2,5 +2,5 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 bin/arduino-cli core install arduino:mbed
 bin/arduino-cli lib install Arduino_LSM9DS1@1.1.0
 mkdir -p $HOME/Arduino/libraries && cp -r $TENSORFLOW_PATH $HOME/Arduino/libraries
-sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/system_setup.cpp
-bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/magic_wand $HOME/Arduino/libraries/tensorflow/examples/magic_wand
+sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tflite-micro/src/tensorflow/lite/micro/arduino/system_setup.cpp
+bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/magic_wand $HOME/Arduino/libraries/tflite-micro/examples/magic_wand
--- examples/person-detection/build.sh
+++ examples/person-detection/build.sh
@@ -3,6 +3,6 @@ bin/arduino-cli core install arduino:mbed
 bin/arduino-cli lib install JPEGDecoder@1.8.0
 git clone --quiet https://github.com/ArduCAM/Arduino.git && cp -r Arduino/ArduCAM $HOME/Arduino/libraries
 mkdir -p $HOME/Arduino/libraries && cp -r $TENSORFLOW_PATH $HOME/Arduino/libraries
-sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/system_setup.cpp
+sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tflite-micro/src/tensorflow/lite/micro/arduino/system_setup.cpp
  > $HOME/Arduino/libraries/JPEGDecoder/src/User_Config.h
-bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/person_detection $HOME/Arduino/libraries/tensorflow/examples/person_detection
+bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/person_detection $HOME/Arduino/libraries/tflite-micro/examples/person_detection
