61a9e26dc52696ee0722fdddca4ef22d4fbd018e
Wojciech Meller <wmeller@antmicro.com>
2021-06-09 17:45:39 +0200

    Change tensorflow to tflite-micro

--- examples/micro-speech/build.sh
+++ examples/micro-speech/build.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 bin/arduino-cli core install arduino:mbed
 mkdir -p $HOME/Arduino/libraries && cp -r $TENSORFLOW_PATH $HOME/Arduino/libraries
-sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tensorflow/src/tensorflow/lite/micro/arduino/system_setup.cpp
-bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/micro_speech $HOME/Arduino/libraries/tensorflow/examples/micro_speech
+sed -i'' '/#define DEBUG_SERIAL_OBJECT/s/(Serial)/(Serial1)/' $HOME/Arduino/libraries/tflite-micro/src/tensorflow/lite/micro/arduino/system_setup.cpp
+bin/arduino-cli compile -b arduino:mbed:nano33ble --output-dir binaries/micro_speech $HOME/Arduino/libraries/tflite-micro/examples/micro_speech
