50d3bc06824baea9d57e1dd438d4f88d8cca4ecf
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-07-06 02:16:18 +0200

    Add navigation images to notebooks

--- examples/micro-speech/micro_speech.py
+++ examples/micro-speech/micro_speech.py
@@ -3,13 +3,13 @@
 ![Renode](https://dl.antmicro.com/projects/renode/renode.png)
 <table align="left">
   <td>
-    <a target="_blank" href="https://colab.research.google.com/github/antmicro/tensorflow-arduino-examples/blob/master/examples/micro-speech/$NOTEBOOK.ipynb"><img src="https://www.tensorflow.org/images/colab_logo_32px.png" />Run in Google Colab</a>
+    <a target="_blank" href="https://colab.research.google.com/github/antmicro/tensorflow-arduino-examples/blob/master/examples/micro-speech/$NOTEBOOK.ipynb"><img src="https://github.com/antmicro/tensorflow-arduino-examples/blob/master/examples/.static/view-in-colab.py" />Run in Google Colab</a>
   </td>
   <td>
-    <a target="_blank" href="https://github.com/antmicro/tensorflow-arduino-examples/blob/master/examples/micro-speech/$NOTEBOOK.ipynb"><img src="https://www.tensorflow.org/images/GitHub-Mark-32px.png" />View ipynb on GitHub</a>
+    <a target="_blank" href="https://github.com/antmicro/tensorflow-arduino-examples/blob/master/examples/micro-speech/$NOTEBOOK.ipynb"><img src="https://github.com/antmicro/tensorflow-arduino-examples/blob/master/examples/.static/view-ipynb.png" />View ipynb on GitHub</a>
   </td>
   <td>
-    <a target="_blank" href="https://github.com/antmicro/tensorflow-arduino-examples/blob/master/examples/micro-speech/micro_speech.py"><img src="https://www.tensorflow.org/images/GitHub-Mark-32px.png" />View Python source on GitHub</a>
+    <a target="_blank" href="https://github.com/antmicro/tensorflow-arduino-examples/blob/master/examples/micro-speech/micro_speech.py"><img src="https://github.com/antmicro/tensorflow-arduino-examples/blob/master/examples/.static/view-source.png" />View Python source on GitHub</a>
   </td>
 </table>
 """
